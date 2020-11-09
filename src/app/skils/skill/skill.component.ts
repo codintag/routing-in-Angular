20c3cb5a7bd170b5b7c34c0751c5b349c5e4dc74
codintag <codintag@gmail.com>
2020-11-09 22:42:09 +0000

    Observable Routes

--- src/app/skils/skill/skill.component.ts
+++ src/app/skils/skill/skill.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { skills } from 'src/app/skills';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { skills } from "src/app/skills";
 
 @Component({
-  selector: 'app-skill',
-  templateUrl: './skill.component.html',
-  styleUrls: ['./skill.component.css']
+  selector: "app-skill",
+  templateUrl: "./skill.component.html",
+  styleUrls: ["./skill.component.css"],
 })
 export class SkillComponent implements OnInit {
-
   selectedSkill;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id'); // get the params "skills/:id"
-    console.log(id);
-    this.selectedSkill = skills.find(skill => skill.id === id);
-  }
+    // let id = +this.route.snapshot.paramMap.get('id'); // get the params "skills/:id"
+    // console.log(id);
+    // this.selectedSkill = skills.find(skill => skill.id === id);
 
+    // this method use Observables, (promise) params changes everytime you change the params, but the method upside run just one time. (because ngOninit run just one time!) || uncomment codes above to see the effect.
+
+    // this method return a promise (observable)
+    this.route.paramMap.subscribe((params) => {
+      let id = +params.get('id');
+      this.selectedSkill = skills.find(skill => skill.id === id);
+    });
+  }
 }
